d82078cd7bd8fd5ce7d9214b90ea844fafee6508
realsameek <realsameek@gmail.com>
2026-08-29 10:45:40 +0500

    auth router

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -3,8 +3,21 @@ import { getAuth } from "firebase-admin/auth"
 export const login = async (req,res)=>{
     try {
         const {token} = req.body
-        const decoded = getAuth(app).verifyIdToken(token)
+        const decoded =await getAuth(app).verifyIdToken(token)
         console.log(decoded)
+        let user = await User.findOne({
+            firebaseUid:decoded.uid
+        })
+         if(!user){
+            user = await User.create({
+                firebaseUid:decoded.uid,
+                name:decoded.name,
+                email:decoded.email,
+                avatar:decoded.picture
+            })
+         }
+
+
         return res.json({decoded})
 
     } catch (error) {
