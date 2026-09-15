b790b4c4bd62a3276aa2ef6e62d5cc30a12df29d
muh-amjad <muhammadamjad3583@gmail.com>
2026-09-15 21:00:00 +0500

    designing Activity bar icon style

--- frontend/src/pages/ProjectPage.jsx
+++ frontend/src/pages/ProjectPage.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import TopBar from '../components/TopBar'
 import { getProject } from '../features/project'
 import { setCurrentProject } from '../redux/projectSlice'
+import ActivityBar from '../components/ActivityBar'
 
 function ProjectPage() {
   const { id } = useParams()
@@ -26,6 +27,9 @@ function ProjectPage() {
         <div className='pointer-events-none absolute -top-40 left-1/3 h-96 w-96 rounded-full bg-sky-500/10 blur-[140px]'/>
         <div className='pointer-events-none absolute -top-20 right-1/4 h-80 w-80 rounded-full bg-violet-500/10 blur-[140px]'/>
         <TopBar/>
+        <div className='flex flex-1 overflow-hidden'>
+           <ActivityBar/>
+        </div>
     </div>
   )
 }
